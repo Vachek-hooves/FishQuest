ebfd6d7c6d13f19b329e00fda7f8ffbb56cd6295
Vachek-hooves <vachek0602@gmail.com>
2024-10-04 12:49:41 +0300

    update season data

--- data/season.js
+++ data/season.js
@@ -1,22 +1,22 @@
 export const fishSeason = [
   {
     season: 'spring',
-    fish: ['catfish', 'perch', 'pike'],
+    fish: ['1', '3', '6','12','15','19'],
     image: require('../assets/image/seasons/spring.jpg'),
   },
   {
     season: 'summer',
-    fish: ['catfish', 'perch', 'pike'],
+    fish: ['2', '4', '5','13','20','21'],
     image: require('../assets/image/seasons/summer.jpg'),
   },
   {
     season: 'autumn',
-    fish: ['catfish', 'perch', 'pike'],
+    fish: ['7', '8', '9','14','16','18'],
     image: require('../assets/image/seasons/autumn.jpg'),
   },
   {
     season: 'winter',
-    fish: ['catfish', 'perch', 'pike'],
+    fish: ['10', '11', '17','6','4','10'],
     image: require('../assets/image/seasons/winter.jpg'),
   },
 ];
