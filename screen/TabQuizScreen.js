5f95dd9cf149298453c4ec824366434bbfee2aa3
Vachek-hooves <vachek0602@gmail.com>
2024-10-06 20:22:44 +0300

    update tab bar

--- screen/TabQuizScreen.js
+++ screen/TabQuizScreen.js
@@ -2,7 +2,7 @@ import {StyleSheet, Text, View, ImageBackground} from 'react-native';
 import {MainLayout} from '../components/Layout';
 
 const TabQuizScreen = () => {
-  return <MainLayout></MainLayout>;
+  return <MainLayout blur={40}></MainLayout>;
 };
 
 export default TabQuizScreen;
